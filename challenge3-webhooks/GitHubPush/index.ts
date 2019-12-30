91d75fca63b35f7e2d4e6e38f15289b9c1128a1f
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2019-12-30 17:27:36 +0100

    Add messages to queue

--- challenge3-webhooks/GitHubPush/index.ts
+++ challenge3-webhooks/GitHubPush/index.ts
@@ -4,11 +4,8 @@ const httpTrigger: AzureFunction = async function(
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  context.log("HTTP trigger function processed a request.");
   if (isPushEvent(req.body)) {
-    context.res = {
-      body: getAddedPngPictures(req.body)
-    };
+    context.bindings.outputQueueItems = getQueueMessages(req.body);
   } else {
     context.res = {
       status: 400,
@@ -18,23 +15,49 @@ const httpTrigger: AzureFunction = async function(
 };
 
 interface PushEvent {
-  commits: [
-    {
-      added: string[];
-    }
-  ];
+  repository: Repository;
+  commits: Commit[];
+}
+
+interface Repository {
+  full_name: string;
+}
+
+interface Commit {
+  id: string;
+  tree_id: string;
+  added: string[];
+}
+
+interface PictureCommittedMessage {
+  repositoryName: string;
+  commitId: string;
+  treeId: string;
+  path: string;
 }
 
 function isPushEvent(body: any): body is PushEvent {
-  return "commits" in body;
+  return "repository" in body && "commits" in body;
 }
 
-function getAddedPngPictures(pushEvent: PushEvent): string[] {
-  return getAddedFiles(pushEvent).filter(isPng);
+function getQueueMessages(pushEvent: PushEvent): PictureCommittedMessage[] {
+  return new Array<PictureCommittedMessage>().concat(
+    ...pushEvent.commits.map(c =>
+      getQueueMessagesForCommit(c, pushEvent.repository)
+    )
+  );
 }
 
-function getAddedFiles(pushEvent: PushEvent): string[] {
-  return new Array<string>().concat(...pushEvent.commits.map(c => c.added));
+function getQueueMessagesForCommit(
+  commit: Commit,
+  repository: Repository
+): PictureCommittedMessage[] {
+  return commit.added.filter(isPng).map(path => ({
+    repositoryName: repository.full_name,
+    commitId: commit.id,
+    treeId: commit.tree_id,
+    path
+  }));
 }
 
 function isPng(path: string): boolean {
